9cb54e946ab89d9ffbb4213ea4ad1f63f823c119
24MW <114576879+riczart@users.noreply.github.com>
2022-10-05 11:02:15 +0200

    Wykonane zadania z KartaPracy2.pdf

---
+++ KartaPracy2.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KartaPracy2.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+    {
+            // Zad 1
+            /*
+            int a = int.Parse(Console.ReadLine());
+            if (a % 3 == 0)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }
+            */
+
+            // Zad 2
+            /*
+            int a = int.Parse(Console.ReadLine());
+            if (a >= 100 && a < 1000 && a % 17 == 0)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }
+            */
+
+            /*           zad 3
+                           int a = int.Parse(Console.ReadLine());
+                           if (a >= 18)
+                           {
+                               Console.WriteLine("TAK");
+                           }
+                           else
+                           {
+                               Console.WriteLine("NIE");
+                           }
+                           Console.ReadKey();*/
+            /*
+                        zad 4
+                        const int limit = 20;
+                        int waga = int.Parse(Console.ReadLine());
+                        if (waga <= limit)
+                        {
+                            Console.WriteLine("tak mozesz jechac");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Lepiej nie jedz");
+                        }*/
+
+            /*           zad5
+             *           int a = int.Parse(Console.ReadLine());
+                        int b = int.Parse(Console.ReadLine());
+                        int c = int.Parse(Console.ReadLine());
+
+                        if ((a<c) & (b > c) || (b < c) & (a > c))
+                        {
+                            Console.WriteLine("TAK miesci sie");
+                        }
+                        else
+                        {
+                            Console.WriteLine("NIE nie miesci sie");
+                        }*/
+
+            /*            zad6
+                        int a = int.Parse(Console.ReadLine());
+                        int p = int.Parse(Console.ReadLine());
+                        if ((Math.Pow(a, p) - a) % p == 0)
+                        {
+                            Console.WriteLine("TAK");
+                        }
+                        else
+                        {
+                            Console.WriteLine("NIE");
+                        }*/
+
+/*            zad 7
+            int p = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+
+            if((p + 3*s) >=k)
+            {
+                Console.WriteLine("Tak, zaba da rade");
+            }
+            else
+            {
+                Console.WriteLine("Nie, nie da rady");
+            }*/
+        }
+    }
+}
+   
