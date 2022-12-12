d5e76999174a5e009bfe343177fc2c6c015429de
24MW <114576879+riczart@users.noreply.github.com>
2022-12-12 09:32:32 +0100

    wykonano KartaPracyO1.pdf

---
+++ KartaPracyO1.cs
@@ -0,0 +1,92 @@
+using System;
+
+namespace ConsoleApp5
+{
+    class Producent
+    {
+        private string nazwa;
+        private string nip;
+        
+        public string getNazwa()
+        {
+            return "nazwa";
+        }
+        public string getNip()
+        {
+            return "nip";
+        }
+        public void setNazwa()
+        {
+           return "nazwa"; 
+        }
+    }
+
+    class Material 
+    {
+        private string typ;
+        private int id;
+        private double cena;
+
+        public void setType() 
+        {
+            return "type";
+        }
+        public string getType()
+        {
+            return "type";
+        }
+        public double getCena()
+        {
+            return "cena";
+        }
+        public void setId()
+        {
+            return "id";
+        }
+    }
+    class Rower 
+    {   
+        public Producent producent;
+        public Material material;
+        private string Producent;
+        private string Kolor;
+        private string Material;
+
+        public void jedz()
+        {
+            return "Jade";
+        }
+
+        public void setColor(string aKolor)
+        {
+            this.Kolor = aKolor;
+        }
+
+        public string getColor()
+        {
+            return Kolor;
+        }
+
+        public void setMaterial(Material amaterial)
+        {
+            this.material = amaterial;
+        }
+
+        public Rower(producent aproducent)
+        {
+            this.producent = aproducent;
+        }
+    }
+    class LibraryManagmentSystem
+    {
+        static void Main(string[] args)
+        {
+            Producent p1 = new Producent();
+
+            Rower r1 = new Rower(p1);
+            material m1 = new Material();
+            r1.setMaterial(m1);
+            
+        }
+    }
+}
